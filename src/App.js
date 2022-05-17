095ca872ff7973cdbd277efbfa3a3b1ca565645e
Ankit Aggarwal <65785638+Ankit27Agg@users.noreply.github.com>
2022-05-17 18:20:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,3 +23,34 @@ function App() {
 }
 
 export default App;
+
+
+
+// //************************  GET EVENTS FROM HTML  *********************************** */
+// const phoneNo = document.getElementById('phone-no');
+// const email = document.getElementById('email');
+
+
+// //**********************************  VALIDATIONS   *************************** */
+// const validation = () => {
+//   let phoneVal = /^[0-9]+$/;
+//   let emailVal = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+// //                                 for phone number
+
+//   if(!phoneVal.test(phoneNo.value)){
+//     alert('Wrong Input OR field is empty');
+//     return false;
+//   }
+
+// //                                 for email 
+//   if(!emailVal.test(email.value)){
+//     alert('Wrong Input OR field is empty');
+//     return false;
+//   }
+
+//   return true;
+// }
+
+
